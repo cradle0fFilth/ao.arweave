c47a66accec8ebbf964e3966794023fb01630a5f
cradle0fFilth <3175806342@11.com>
2024-03-11 16:02:24 +0800

    use nvm

--- ao.sh
+++ ao.sh
@@ -5,6 +5,7 @@ function install_ao(){
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     nvm install 20
+    nvm use 20
     npm i -g https://get_ao.g8way.io
 }
 
